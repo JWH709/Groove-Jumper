3362ea57726ef9776e518d694348cdac6f89d2ac
John Heneghan <jwheneghan@hotmail.com>
2024-01-15 15:20:40 -0330

    Add functions for filtering data

--- script.js
+++ script.js
@@ -4,12 +4,74 @@ fetch(getRandomStartPointer())
   })
   .then((jsonObj) => {
     console.log(jsonObj);
+    getFilteredAlbum(jsonObj);
   })
   .catch((error) => {
     console.log(error);
   });
 
-//Code that determines the start point
+//create objects to store & reference information globally:
+let albumsGlobal = new Array();
+
+//Create consturctors:
+
+function Album(
+  albumPosition,
+  album,
+  artists,
+  extraartists,
+  labels,
+  styles,
+  year,
+  connectionNumber
+) {
+  (this.albumPosition = albumPosition),
+    (this.album = album),
+    (this.artists = artists),
+    (this.extraartists = extraartists),
+    (this.labels = labels),
+    (this.styles = styles),
+    (this.year = year),
+    (this.connectionNumber = connectionNumber);
+}
+
+function Arrays(name, id) {
+  (this.name = name), (this.id = id);
+}
+
+//Create functions that filter the information need for the constructors:
+
+function indexerFilter(currentItem) {
+  for (let i = 0; i < currentItem.length; i++) {
+    let filteredResults = new Array();
+    filteredResults.push(new Arrays(currentItem[i].name, currentItem[i].id));
+    return filteredResults;
+  }
+}
+function getAlbumCount() {
+  if (albumsGlobal == undefined) {
+    return 0;
+  } else {
+    return albumsGlobal.length++;
+  }
+}
+//Functions that will then use the constructors to build an object:
+
+function getFilteredAlbum(parsedJSON) {
+  let albumPosition = getAlbumCount();
+  let album = parsedJSON.title;
+  let artists = indexerFilter(parsedJSON.artists);
+  let extraartists = indexerFilter(parsedJSON.extraartists);
+  let labels = indexerFilter(parsedJSON.labels);
+  let styles = parsedJSON.styles;
+  let year = parsedJSON.year;
+  albumsGlobal.pop();
+  albumsGlobal.push(
+    new Album(albumPosition, album, artists, extraartists, labels, styles, year)
+  );
+}
+
+//Code that determines the start point:
 
 //I should make the album pool bigger, but before I do that I should get the app working from one of these two points
 
